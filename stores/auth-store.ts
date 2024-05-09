4b77b3faa2df04ec6e7f4406dd543aa92dcc7005
Lars Rickert <dev@lars-rickert.de>
2024-05-09 20:59:57 +0200

    fix username

--- stores/auth-store.ts
+++ stores/auth-store.ts
@@ -4,7 +4,10 @@ export const useAuthStore = defineStore("auth", () => {
   /**
    * Current username. Will be synched with the localStorage.
    */
-  const username = useLocalStorage("username", "");
+  const username = useLocalStorage("username", "", {
+    writeDefaults: false,
+    initOnMounted: true,
+  });
 
   return { username };
 });
